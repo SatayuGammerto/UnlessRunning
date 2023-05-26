a428d1e97842a2acc88aea5fe3351412dad3ebe5
SatayuPat <satayuamp123@gmail.com>
2023-05-26 16:43:26 +0700

    jump

--- Unless Running/Assets/Script/PlayerCtrl.cs
+++ Unless Running/Assets/Script/PlayerCtrl.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    [SerializeField] float gravity;
+    [SerializeField] Vector2 velocity;
+    [SerializeField] float jumpVelocity;
+    [SerializeField] public float groundHeight = 10f;
+    [SerializeField] public bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,31 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+        if (!isGrounded)
+        {
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            velocity.y += gravity * Time.fixedDeltaTime;
+
+            if (pos.y <= groundHeight)
+            {
+                pos.y = groundHeight;
+                isGrounded = true;
+            }
+        }
+
+        transform.position = pos; 
     }
 }
